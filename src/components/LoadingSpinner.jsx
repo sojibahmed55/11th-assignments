67f4d342135b6c6e2928ba449e9791a5f962880e
SojibAhmed1 <sojibahmed514698@gmail.com>
2025-08-12 22:30:16 +0600

    added done

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -7,14 +7,6 @@ const LoadingSpinner = () => {
             <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-green-700"></div>
             <span className="ml-4 text-xl font-semibold text-gray-700">Loading ...</span>
         </div>
-
-
-
-
-
-
-
-
     );
 };
 
